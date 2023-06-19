fc315463361db89d5f86138cae803689432f343a
oludayoadedayo <oludayoadedayo@gmail.com>
2023-06-19 12:23:53 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * programs that prints with puts function
- * return: 0 (success)
+ * main - programs that prints with puts function
+ * Return: 0 (Success)
  */
 int main(void)
 {
