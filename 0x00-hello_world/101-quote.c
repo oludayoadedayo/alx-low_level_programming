7a5393719538efd9515de9c12e4939e274a49e9d
oludayoadedayo <oludayoadedayo@gmail.com>
2023-06-20 00:45:29 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 #include <unistd.h>
 /**
 * main - A c program that prints a line with standard error
-* Return: 1 (Success)
+* Return: Always 1 (Success)
 */
 int main(void)
 {
-	write(2,
-	"and that piece of art is useful\" - Dora Korpar, 2015-10-19)\n", 59);
+write(
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19)\n", 59);
 return (1);
 }
