acf105efbe0d43915b13b322e42afc5f66fe6304
oludayoadedayo <oludayoadedayo@gmail.com>
2023-06-20 08:03:45 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Progran that prints
+ * all possible combinations of single-digit numbers
+ * Return: 0 value
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar((n % 10) + '0');
+		if (n == 9)
+			continue;
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
