160b203839777ba2d7151def5087fe862311c656
oludayoadedayo <oludayoadedayo@gmail.com>
2023-06-20 07:22:13 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints the lowercase alphabet in reverse,
+ * followed by a new line
+ * Return: 0 value
+ */
+int main(void)
+{
+	char lowercase alpbt;
+
+	for (lowercase alpbt = 'z'; lowercase alpbt >= 'a'; lowercase alpbt--)
+		putchar(lowercase alpbt);
+
+	putchar('\n');
+	return (0);
+}
