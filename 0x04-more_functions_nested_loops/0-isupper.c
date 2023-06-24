82a7a973738bd50df1992683346f12bfa72839f3
oludayoadedayo <oludayoadedayo@gmail.com>
2023-06-23 20:29:55 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,15 +2,15 @@
 
 /**
  * _isupper - Function that check if a letter is uppercase
- * @x: The number to be checked
+ * @c: The number to be checked
  * Return: 1 for upper letter or 0 for any else
  */
 
-int _isupper(int x)
+int _isupper(int c)
 
 {
 
-	if (x >= 65 && x <= 90)
+	if (c >= 65 && c <= 90)
 	{
 		return (1);
 	}
