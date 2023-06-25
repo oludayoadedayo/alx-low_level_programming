98803aa38b0b9d33ee1ecf939e90f1f755395432
oludayoadedayo <oludayoadedayo@gmail.com>
2023-06-25 03:03:01 -0700

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,13 +16,13 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	else if (b > a && b > c)
-	{
-		largest = b;
-	}
-	else if (c > a && c > b)
+	else if (a > b && c > a)
 	{
 		largest = c;
+	}
+	else if (b > c)
+	{
+		largest = b;
 	}
 	else
 	{
